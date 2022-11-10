512c456dbc40db402346b758a74785bfed8c5654
Domain835 <domain835@gmail.com>
2022-11-10 19:34:20 +0600

    13th lesson homework completed

---
+++ lesson_13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Пожалуйста ведите число");
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Данное число четное");
+            }
+            else
+            {
+                Console.WriteLine("Данное число не четное");
+            }
+
+            
+        }
+    }
+}
