4ca5b2a4cf7895bc2d761f7907a16d1466f8d7c9
Domain835 <domain835@gmail.com>
2022-11-14 12:05:35 +0600

    Added sum counter for even and odd counters

--- lesson_16/Program.cs
+++ lesson_16/Program.cs
@@ -9,23 +9,39 @@
             int amountOfEvenNumbers = 0;
             int amountOfOddNumbers = 0;
 
-            Console.WriteLine("Рожалуйста ведите первое число диапазона");
+            int sumOfEvenNumbers = 0;
+            int sumOfOddNumbers = 0;
+
+            Console.WriteLine("Пожалуйста ведите первое число диапазона");
             firstValue = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Рожалуйста ведите второе число диапазона");
+            Console.WriteLine("Пожалуйста ведите второе число диапазона");
             secondValue = int.Parse(Console.ReadLine());
 
-            while (firstValue < secondValue)
+            while (firstValue <= secondValue)
             {
                 if (firstValue % 2 == 0)
+                {
                     amountOfEvenNumbers++;
+                    sumOfEvenNumbers = sumOfEvenNumbers + firstValue;
+                }
+
                 else
+                {
                     amountOfOddNumbers++;
+                    sumOfOddNumbers += firstValue;
+                }
+                    
                 firstValue++;
             }
 
             Console.WriteLine("Количество четных чисел в диапазоне:" + amountOfEvenNumbers);
             Console.WriteLine("Количество не четных чисел в диапазоне:" + amountOfOddNumbers);
+
+            Console.WriteLine("Сумма четных чисел в диапазоне:" + sumOfEvenNumbers);
+            Console.WriteLine("Сумма не четных чисел в диапазоне:" + sumOfOddNumbers);
+
+            Console.ReadLine();
         }            
     }
 }
