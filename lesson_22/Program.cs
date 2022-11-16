6c21bbd881e6c6ca3cc77518f62e8d550e6dd5a5
Domain835 <domain835@gmail.com>
2022-11-16 16:03:25 +0600

    Homework of lesson 22 (triangles) completed

---
+++ lesson_22/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            for (int k = 0; k < 2; k++)
+            {
+                for (; i < 10; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                for (; i > 0; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
