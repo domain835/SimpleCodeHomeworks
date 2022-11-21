33f7a3ca667389cb61b37ebbdf87ea744db493e2
Domain835 <domain835@gmail.com>
2022-11-21 12:01:54 +0600

    lesson 26 (enter array from keyboard)

---
+++ lesson_26/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Homework
+{
+    /* Ввод массива с клавиатуры */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Пожалуйста ведите количество элементов в массиве: ");
+
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            int[] array = new int[elementsCount];
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Пожалуйста введите элемент массива под индексом {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nВывод массива: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
