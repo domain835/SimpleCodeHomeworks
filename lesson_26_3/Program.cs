7b1abce0ac27ffcd58c1bc7f5988b321df85c5bb
Domain835 <domain835@gmail.com>
2022-11-21 15:29:46 +0600

    lesson 26_3 (sum of even numbers in array)

---
+++ lesson_26_3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Найти сумму четных чисел в мвссиве */
+
+            int sum = 0;
+
+            Console.Write("Пожалуйста ведите количество элементов в массиве: ");
+
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            int[] array = new int[elementsCount];
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Пожалуйста введите элемент массива под индексом {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nСумма четных чисел: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    sum +=array[i];
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
