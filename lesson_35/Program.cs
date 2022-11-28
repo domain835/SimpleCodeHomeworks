b33757ec695fa62e00912d3871df1b2fa644de2f
Domain835 <domain835@gmail.com>
2022-11-28 23:22:05 +0600

    lesson_35_1 my solution

---
+++ lesson_35/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static string CreateString(string symbol, int count)
+        {
+            string result = "";
+
+            for (int i = 0; i < count; i++)
+            {
+                result = result + symbol;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Пожалуйста введите символ: ");
+            string symbol = Console.ReadLine();
+
+            Console.Write("Пожалуйста введите количество: ");
+            int count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CreateString(symbol, count));
+            
+
+          /*  for (int i = 0; i < count; i++)
+            {
+                Console.Write(symbol);
+            }*/
+
+        }
+    }
+}
