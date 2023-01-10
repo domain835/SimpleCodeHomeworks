39442e0cada36acfdb8fc36079685936fc874bf2
Domain835 <domain835@gmail.com>
2023-01-10 21:28:51 +0600

    homework 13, adding value by index, to start, to end array

---
+++ lesson_43_2(pl_58)_hw_13/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        /* Adding value by index */
+        static void Insert(ref int[] array, int value, int index)
+        {
+            int[] newArray = new int[array.Length + 1];
+
+            newArray[index] = value;
+
+            for (int i = 0; i < index; i++)
+                newArray[i] = array[i];
+
+            for (int i = index; i < array.Length; i++)
+                newArray[i + 1] = array[i];
+
+            array = newArray;
+        }
+
+        /* Adding value to start array */
+        static void AddFirst(ref int[] arr, int value)
+        {
+            Insert(ref arr, value, 0);
+        }
+
+        /* Adding value to end array */
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 56, 32, 88 };
+
+            AddFirst(ref arr, -5);
+        }
+    }
+}
