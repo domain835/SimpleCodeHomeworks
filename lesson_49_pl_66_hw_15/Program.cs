d4c417a54bc43d206dc3681970f5e2e6e0286c68
Domain835 <domain835@gmail.com>
2023-01-19 09:31:23 +0600

    Home work 15 (Iterate array recursively) completed

---
+++ lesson_49_pl_66_hw_15/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static void myFunc(int[] array, int i = 0)
+        {
+            if (i < array.Length)
+            {
+                Console.WriteLine(array[i]);
+                myFunc(array, i + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 56, 32, 88 };
+
+            myFunc(myArray);
+        }
+    }
+}
+
