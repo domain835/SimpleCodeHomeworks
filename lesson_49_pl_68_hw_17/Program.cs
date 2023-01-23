22f83c1497ff6756b9eff1d064c11f83b0918978
Domain835 <domain835@gmail.com>
2023-01-23 19:40:51 +0600

    home work 17 (addition digits of number) completed

---
+++ lesson_49_pl_68_hw_17/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static int CreateSum(int number)
+        {
+            //if (number < 10)
+            //    return number;
+
+            //int lastDigit = number % 10;
+
+            //int remainingNumber = number / 10;
+
+            //return lastDigit + CreateSum(remainingNumber);
+
+            /* Loop soluion */
+
+            int result = 0;
+
+            while (number > 0)
+            {
+                result += number % 10;
+                number /= 10;
+            }
+
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            int number = 735;
+
+            int result = CreateSum(number);
+        }
+    }
+}
