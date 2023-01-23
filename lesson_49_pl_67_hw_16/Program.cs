a99305dd963818e108f0575bb35ec736a5eeee1d
Domain835 <domain835@gmail.com>
2023-01-23 18:25:54 +0600

    home work 16 (addition numbers by recursion) completed

---
+++ lesson_49_pl_67_hw_16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static int Sum(int[] array, int i = 0)
+        {
+            if (i >= array.Length)
+               return 0;
+
+            int result = Sum(array, i + 1);
+
+            return array[i] + result;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = {2, 3, 1};
+
+            int result = Sum(myArray);
+        }
+    }
+}
